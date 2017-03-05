6656def51f739b63c92defd2cebd0c3c96ef6235
Ravi Tiwari <rtiwari@digite.com>
2017-03-05 15:14:14 +0530

    Finished basic version of scrollview

--- PageTheScroll/MainVC.swift
+++ PageTheScroll/MainVC.swift
@@ -10,18 +10,31 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     var images = [UIImageView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        for x in 0...2{
-            let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
-        }
-        
-        print("Count : \(images.count)")
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        let scrollViewWidth = scrollView.frame.size.width
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            let newX: CGFloat = (scrollViewWidth / 2) + (scrollViewWidth * CGFloat(x))
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75 , y: ((scrollView.frame.size.height / 2) - 75) , width: 150, height: 150)
+            contentWidth = scrollViewWidth * CGFloat(x+1)
+        }
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: 150)
+    }
 }
 
