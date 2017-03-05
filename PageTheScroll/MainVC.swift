3d52b15c14307fde531ff391cae402be0c3195db
Ravi Tiwari <rtiwari@digite.com>
2017-03-05 13:03:47 +0530

    Adding Assets and rearranging structure

---
+++ PageTheScroll/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  MainVC.swift
+//  PageTheScroll
+//
+//  Created by Ravi Tiwari on 3/4/17.
+//  Copyright © 2017 SelfStudy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
